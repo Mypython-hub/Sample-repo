8eea73b7476795c498bacd666b9a8b17d9c08b3d
Madhusmita Pattanayak <madhusmitapattanayak@gmail.com>
2019-07-17 17:13:09 -0400

    added hello message

--- scraper.py
+++ scraper.py
@@ -7,6 +7,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
 }
 
+print("hello")
+
 
 def check_price():
 
